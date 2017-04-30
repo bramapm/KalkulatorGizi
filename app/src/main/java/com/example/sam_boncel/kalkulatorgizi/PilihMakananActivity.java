3b29c6f014f2cb551c32a42bf303919a189966b3
bncl <muhammadfiqhifahmi@yahoo.co.id>
2017-04-30 17:47:03 +0700

    popup pilih makanan
    kebekan memory, close program -_-

---
+++ app/src/main/java/com/example/sam_boncel/kalkulatorgizi/PilihMakananActivity.java
@@ -0,0 +1,13 @@
+package com.example.sam_boncel.kalkulatorgizi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PilihMakananActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilih_makanan);
+    }
+}
